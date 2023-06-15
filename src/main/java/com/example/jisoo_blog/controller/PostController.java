15d2a8808f389fdb90ee03a5fd9536b8b4a132bd
JisooPyo <audwl326@gmail.com>
2023-06-16 04:10:15 +0900

    Update - 수정된 게시글 return되게끔 수정.

--- src/main/java/com/example/jisoo_blog/controller/PostController.java
+++ src/main/java/com/example/jisoo_blog/controller/PostController.java
@@ -60,7 +60,7 @@ public class PostController {
 
     // http://localhost:8080/JisooBlog/memos?id=1&password=1234
     @PutMapping( "/posts" )
-    public void updatePost( @RequestParam Long id, @RequestParam String password, @RequestBody PostRequestDto requestDto ) {
+    public PostResponseDto updatePost( @RequestParam Long id, @RequestParam String password, @RequestBody PostRequestDto requestDto ) {
         // 해당 글 DB에 존재하는 지 확인
         if ( !postList.containsKey( id ) ) {
             throw new IllegalArgumentException( "해당 글이 존재하지 않습니다." );
@@ -77,6 +77,9 @@ public class PostController {
 
         // 글 수정
         post.update( requestDto );
+        return new PostResponseDto( post );
 
     }
+
+
 }

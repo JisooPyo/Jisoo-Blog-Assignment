d7615a55b0b816327a82fee904fb960fb5df3679
JisooPyo <audwl326@gmail.com>
2023-06-16 00:12:21 +0900

    Jisoo Blog 프로젝트 만들기

---
+++ src/main/java/com/example/jisoo_blog/JisooBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.jisoo_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JisooBlogApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( JisooBlogApplication.class, args );
+    }
+
+}

c14b73bb258f7f8804d98ca5e7cb92a0143276ba
JisooPyo <audwl326@gmail.com>
2023-06-29 01:08:14 +0900

    회원가입, 로그인 구현

--- src/main/java/com/example/jisoo_blog/entity/User.java
+++ src/main/java/com/example/jisoo_blog/entity/User.java
@@ -8,14 +8,27 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name="users")
+@Table(name = "users")
 @NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false,unique = true)
+
+    @Column(nullable = false, unique = true)
     private String username;
+
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+
+    public User(String username, String password, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
--- src/main/java/com/example/jisoo_blog/repository/UserRepository.java
+++ src/main/java/com/example/jisoo_blog/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.jisoo_blog.repository;
 import com.example.jisoo_blog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
 }

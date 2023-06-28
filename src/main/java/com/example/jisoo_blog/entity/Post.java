08647512f707de65d66ab21cb572b5dc305c55d9
JisooPyo <audwl326@gmail.com>
2023-06-28 22:36:35 +0900

    Post 클래스 Entity 지정

--- src/main/java/com/example/jisoo_blog/entity/Post.java
+++ src/main/java/com/example/jisoo_blog/entity/Post.java
@@ -1,30 +1,33 @@
 package com.example.jisoo_blog.entity;
 
 import com.example.jisoo_blog.dto.PostRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table
 @NoArgsConstructor
-public class Post {
+public class Post extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String posttitle;
-    private String username;
-    private String password;
+    @Column(nullable = false)
+    private String posttitle, username, password;
+    @Column
     private String contents;
-    private String createdAt;
-    private String modifiedAt;
 
-    public Post( PostRequestDto requestDto ) {
+    public Post(PostRequestDto requestDto) {
         this.posttitle = requestDto.getPosttitle();
         this.username = requestDto.getUsername();
         this.password = requestDto.getPassword();
         this.contents = requestDto.getContents();
     }
 
-    public void update( PostRequestDto requestDto ) {
+    public void update(PostRequestDto requestDto) {
         this.posttitle = requestDto.getPosttitle();
         this.username = requestDto.getUsername();
         this.contents = requestDto.getContents();
